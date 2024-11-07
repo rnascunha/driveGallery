329fce832c9576a4e6fbd1e0a6fde5c0a720eb7f
Rafael Cunha <rnascunha@gmail.com>
2024-11-06 21:58:03 -0300

    Performance improvement.

--- src/appComponents/gdriveGallery/components/displayImageContainer.tsx
+++ src/appComponents/gdriveGallery/components/displayImageContainer.tsx
@@ -39,16 +39,17 @@ export default function DisplayImageContainer({
   }, [dir]);
 
   const images = useMemo(() => {
-    return (
-      imagesFile?.map((img) => ({
-        id: img.id as string,
-        name: img.name as string,
-        url: driveImageURL(img.id as string),
-        thumbnail: driveImageThumbnailURL(img.id as string, 100),
-        description: img.description,
-      }))
-    );
-  }, [imagesFile]);
+    return imagesFile?.map((img) => ({
+      id: img.id as string,
+      name: img.name as string,
+      url:
+        props.maxWidth === 0
+          ? driveImageURL(img.id as string)
+          : driveImageThumbnailURL(img.id as string, props.maxWidth),
+      thumbnail: driveImageThumbnailURL(img.id as string, 100),
+      description: img.description,
+    }));
+  }, [imagesFile, props.maxWidth]);
 
   if (error !== null) return <ImageListError error={error} />;
   if (!dir) return <ImageListIdDefined />;
